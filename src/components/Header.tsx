d7932f97e7e8443b313c75a9f8e9ebef3d98fbd2
keydian <jmg.pinto@campus.fct.unl.pt>
2021-11-20 19:52:58 +0000

    Scuffium Headerium

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,38 @@
 import '../styles/Header.css'
 import Button from '@mui/material/Button'
+import Avatar from '@mui/material/Avatar'
 
 function Header(){
 
     const logoFolder = process.env.PUBLIC_URL+"/logos/";
     return(
-        <div className="Header">
+        <div style={{justifyContent:"space-between"}} className="Header">
             <img 
             src={logoFolder+'Musicbeat-logos_transparent.png'}
             style={{maxWidth:'100%', maxHeight:'100%', float:"left", marginLeft:'1vw'}}
             >
             </img>
             <div style={{display:'inline'}}>
-                <Button>
+                <Button size="large" variant="outlined" className="Button">
                     Search
                 </Button>
-                <Button>
+                <Button variant="outlined" size="large" className="Button">
                     Top Rated
                 </Button>
-                <Button>
+                <Button variant="outlined" size="large" className="Button">
                     My Collections 
                 </Button>
-                <Button>
+                <Button variant="outlined" size="large" className="Button">
                     Jams
                 </Button>
-                <Button>
+                <Button variant="outlined" size="large" className="Button">
                     FAQ
                 </Button>
+                
             </div>
+            <Avatar className="Avatar">
+
+            </Avatar>
         </div>
     );
 }

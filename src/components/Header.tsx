3a42ee41a0199dee8c322b528707f7873858f6af
rvlopes <rv.lopes@campus.fct.unl.pt>
2021-11-28 16:42:38 +0000

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,39 +2,52 @@ import '../styles/Header.css'
 import Button from '@mui/material/Button'
 import Avatar from '@mui/material/Avatar'
 import { useLocation, useNavigate } from "react-router";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { dispatch_to_props, FullProps, state_to_props } from '../redux/redux';
 import { connect } from 'react-redux';
+import { Alert, Snackbar } from '@mui/material';
 
-function Header(Props : FullProps){
-
+function Header(Props: FullProps) {
     let navigate = useNavigate();
     let loc = useLocation()
-    const logoFolder = process.env.PUBLIC_URL+"/logos/";
+    const logoFolder = process.env.PUBLIC_URL + "/logos/";
+    const [open, setOpen] = useState<boolean>(false)
 
     //To not allow the user to go mess around with urls
     useEffect(() => {
-        if(!Props.isLogged) {
-            if(loc.pathname !== "/register" && loc.pathname !== "/login") {
-                if(!localStorage.getItem("token")) {
-                    navigate("/login")
+        if (!Props.isLogged) {
+            if (loc.pathname !== "/register" && loc.pathname !== "/login") {
+                if (!localStorage.getItem("token")) {
+                    navigate("/")
                 }
             }
         }
     }, [Props.isLogged])
 
-    return(
-        <div className="Header" style={{display:'inline'}}>
-            <div style={{justifyContent:"space-evenly", display:"flex"}}>
-            <img 
-            src={logoFolder+'Musicbeat-logos_transparent.png'}
-            style={{maxWidth:'5%', maxHeight:'5%', float:"left", marginTop:"0.5vw"}}
-            onClick={() =>{
-                navigate('../')
-            }}
-            alt="musicbeat-logo"
-            >
-            </img>
+    const handleCloseSnack = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    return (
+        <div className="Header" style={{ display: 'inline' }}>
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleCloseSnack}>
+                <Alert onClose={handleCloseSnack} severity="success" sx={{ width: '100%' }}>
+                   Session Expired!
+                </Alert>
+            </Snackbar>
+            <div style={{ justifyContent: "space-evenly", display: "flex" }}>
+                <img
+                    src={logoFolder + 'Musicbeat-logos_transparent.png'}
+                    style={{ maxWidth: '5%', maxHeight: '5%', float: "left", marginTop: "0.5vw" }}
+                    onClick={() => {
+                        navigate('../')
+                    }}
+                    alt="musicbeat-logo"
+                >
+                </img>
                 <Button size="large" variant="outlined" className="Button">
                     Search
                 </Button>
@@ -42,7 +55,7 @@ function Header(Props : FullProps){
                     Top Rated
                 </Button>
                 <Button variant="outlined" size="large" className="Button">
-                    My Collections 
+                    My Collections
                 </Button>
                 <Button variant="outlined" size="large" className="Button">
                     Jams
@@ -50,9 +63,9 @@ function Header(Props : FullProps){
                 <Button variant="outlined" size="large" className="Button">
                     FAQ
                 </Button>
-                <Avatar className="Avatar" sx={{marginTop:"1vw"}}/>
+                <Avatar className="Avatar" sx={{ marginTop: "1vw" }} />
             </div>
-            
+
         </div>
     );
 }

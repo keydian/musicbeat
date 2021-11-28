4d64dc22d47cf511a16173167b92c18ad3bf87af
keydian <jmg.pinto@campus.fct.unl.pt>
2021-11-28 18:41:16 +0000

    StartPage Shell

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,11 +1,28 @@
+import { Typography } from "@mui/material";
 import { useNavigate } from "react-router";
+import '../styles/StartPage.css';
 
 function StartPage() {
     let navigate = useNavigate();
     
     return (
-        <div>
-           WORK IN PROGRESS
+        <div className = "StartPage">
+           <div className="TitleText">
+            <Typography variant="h5">
+                Discovery Queue
+            </Typography>
+           </div>
+           <div className = "DiscoveryQueue">
+                
+           </div>
+           <div className="TitleText">
+           <Typography variant="h5">
+                Trending
+            </Typography>
+           </div>
+           <div className = "Trending">
+
+           </div>
         </div>
     )
 }

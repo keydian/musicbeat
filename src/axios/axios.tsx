b948a05afac1ef17a66dad6e94dfb1899bc7cc00
Manded <f.pisco@campus.fct.unl.pt>
2021-12-05 18:17:28 +0000

    logout not working

--- src/axios/axios.tsx
+++ src/axios/axios.tsx
@@ -2,8 +2,8 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import store, { logout, reset_token } from "../redux/redux";
 import { CreateCollection, LoginCreds, RegisterCreds } from "../types/types";
 
-//const url = "https://ipm2122appwesteurope55310.azurewebsites.net/rest/";
-const url = "http://localhost:8080/rest/"
+const url = "https://ipm2122appwesteurope55310.azurewebsites.net/rest/";
+//const url = "http://localhost:8080/rest/"
 
 axios.interceptors.request.use(
   function (config: AxiosRequestConfig) {
@@ -44,6 +44,9 @@ export async function loginUser(creds: LoginCreds) {
   return await axios.put(url.concat("users"), creds)
 }
 
+export async function logoutUser() {
+  return await axios.delete(url.concat("users/logout"))
+}
 export async function registerUser(creds: RegisterCreds) {
   return await axios.post(url.concat("users"), creds);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import '../styles/Header.css'
 import Button from '@mui/material/Button'
 import Avatar from '@mui/material/Avatar'
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useLocation, useNavigate } from "react-router";
 import { useEffect } from 'react';
+import { logoutUser} from '../axios/axios';
 import { dispatch_to_props, FullProps, state_to_props } from '../redux/redux';
 import { connect } from 'react-redux';
 
@@ -10,7 +12,24 @@ function Header(Props: FullProps) {
     let navigate = useNavigate();
     let loc = useLocation()
     const logoFolder = process.env.PUBLIC_URL + "/logos/";
+    const handleLogout = () => {
+        sessionStorage.removeItem("token");
+        Props.logout();
+        navigate('');
+      };
     
+      const logout = () => {
+        console.log("BLABLA")
+        logoutUser().then(
+          (response) => {
+            handleLogout();
+          },
+          (error) => {
+            handleLogout();
+            console.log(error);
+          }
+        );
+      };
     //To not allow the user to go mess around with urls
     useEffect(() => {
         if (!Props.isLogged) {
@@ -57,6 +76,7 @@ function Header(Props: FullProps) {
                     FAQ
                 </Button>
                 <Avatar className={"Avatar Clickable"} sx={{ marginTop: "1vw" }} onClick={()=> navigate('/profile/'+Props.username)}/>
+                <LogoutIcon className={"Logout Clickable"} sx={{ marginTop: "1vw" }} onClick={()=> logout}/>
             </div>
 
         </div>

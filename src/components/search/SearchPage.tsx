831b61e4d9fa8ae9605d58f7acb643370b01fdb8
rvlopes <rv.lopes@campus.fct.unl.pt>
2021-12-07 17:48:43 +0000

    search page fix

--- src/components/search/SearchPage.tsx
+++ src/components/search/SearchPage.tsx
@@ -1,32 +1,31 @@
-import {dispatch_to_props, FullProps, state_to_props} from "../../redux/redux";
-import {connect} from "react-redux";
+import { dispatch_to_props, FullProps, state_to_props } from "../../redux/redux";
+import { connect } from "react-redux";
 import { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search'
 import { FormControl, InputLabel, NativeSelect, FormHelperText, TextField, InputAdornment } from "@mui/material";
 
 
-const [searchBy, setBy] = useState<string>("Song");
-const [search, setSearch] = useState<number>(0);
-const [val, setVal] = useState<string>("");
-
-
-const setSearchBy= (
-    eventReact: React.ChangeEvent<unknown>,
-    newBy: string ) =>{
-        setBy(newBy);
-    }
-;
-
-const submitSearch = () =>{
-    setSearch((search) => search+1)
-}
-;
-
 
 //TODO: SEPARATE LISTING COMPONENTS, PAGINATION
+function SearchPage(Props: FullProps) {
+    const [searchBy, setBy] = useState<string>("Song");
+    const [search, setSearch] = useState<number>(0);
+    const [val, setVal] = useState<string>("");
 
-function SearchPage(Props: FullProps){
-    return(
+
+    const setSearchBy = (
+        eventReact: React.ChangeEvent<unknown>,
+        newBy: string) => {
+        setBy(newBy);
+    }
+        ;
+
+    const submitSearch = () => {
+        setSearch((search) => search + 1)
+    }
+        ;
+
+    return (
         <div className="searchPage">
             <div className="searchBody">
                 <FormControl className="selectForm">
@@ -34,17 +33,17 @@ function SearchPage(Props: FullProps){
                         Search By:
                     </InputLabel>
                     <NativeSelect
-                    value={searchBy}
-                    onChange={(e)=>setSearchBy(e, e.target.value)}
-                    inputProps={{
-                        name: "Search by:",
-                        id:"by-helper"
-                    }}
+                        value={searchBy}
+                        onChange={(e) => setSearchBy(e, e.target.value)}
+                        inputProps={{
+                            name: "Search by:",
+                            id: "by-helper"
+                        }}
                     >
                         <option value={"Songs"}>Songs</option>
                         <option value={"Album"}>Album</option>
                         <option value={"Users"}>Users</option>
-                        <option value ={"Collections"}>Collections</option>
+                        <option value={"Collections"}>Collections</option>
                     </NativeSelect>
                     <FormHelperText>
                         Select your preferred search method:
@@ -53,30 +52,30 @@ function SearchPage(Props: FullProps){
             </div>
             <div>
                 <TextField
-                className="searchTextField"
-                id="searchbar"
-                type="search"
-                style={{textAlign:"center"}}
-                inputProps={{
-                    startAdornment:(
-                        <InputAdornment position="start">
-                            <SearchIcon
-                            style={{cursor:"pointer"}}
-                            onClick={() => {
-                                submitSearch();
-                            }}
-                            />
-                        </InputAdornment>
-                    ),
-                }}
-                onChange={(e) => {
-                    setVal(e.target.value);
-                }}
-                onKeyPress={(e)=>{
-                    if(e.key==="Enter"){
-                        submitSearch();
-                    }
-                }}
+                    className="searchTextField"
+                    id="searchbar"
+                    type="search"
+                    style={{ textAlign: "center" }}
+                    inputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon
+                                    style={{ cursor: "pointer" }}
+                                    onClick={() => {
+                                        submitSearch();
+                                    }}
+                                />
+                            </InputAdornment>
+                        ),
+                    }}
+                    onChange={(e) => {
+                        setVal(e.target.value);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                            submitSearch();
+                        }
+                    }}
                 />
             </div>
         </div>
